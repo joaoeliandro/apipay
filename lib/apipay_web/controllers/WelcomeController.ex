f635564279a48be01b824fd030307a6eb19186f8
João Eliandro Germano <eliandrogermano@gmail.com>
2021-02-23 02:43:08 -0300

    Criado um primeiro controller para testes

--- lib/apipay_web/controllers/WelcomeController.ex
+++ lib/apipay_web/controllers/WelcomeController.ex
@@ -1,7 +1,23 @@
 defmodule ApipayWeb.WelcomeController do
   use ApipayWeb, :controller
 
-  def index(conn, _params) do
-    text(conn, "Hello World!")
+  alias Apipay.Numbers
+
+  def index(conn, %{"filename" => filename}) do
+    filename
+    |> Numbers.sum_from_file()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, %{result: result}}, conn) do
+   conn
+   |> put_status(:ok)
+   |> json(%{message: "Welcome to Apipay API. Here is your number #{result}"})
+  end
+
+  defp handle_response({:error, reason}, conn) do
+   conn
+   |> put_status(:bad_request)
+   |> json(reason)
   end
 end
