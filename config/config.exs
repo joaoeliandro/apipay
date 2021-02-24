e2aed4fbf9d85d3eb9e4a451375aaabcb27ecc9c
João Eliandro Germano <eliandrogermano@gmail.com>
2021-02-24 00:31:54 -0300

    Adicionado configurações para o uuid nos ids das entidades

--- config/config.exs
+++ config/config.exs
@@ -18,6 +18,10 @@ config :apipay, ApipayWeb.Endpoint,
   pubsub_server: Apipay.PubSub,
   live_view: [signing_salt: "bXAY50/h"]
 
+config :apipay, Apipay.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreign_key: [type: :binary_id]
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
