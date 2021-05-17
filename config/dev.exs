19f9733f81a025cd0933933cf455b7e164f78e15
joaoeliandro <eliandrogermano@gmail.com>
2021-05-17 05:17:50 -0300

    Changed port to connect on docker postgres

--- config/dev.exs
+++ config/dev.exs
@@ -6,7 +6,7 @@ config :apipay, Apipay.Repo,
   password: "postgres",
   database: "apipay_dev",
   hostname: "localhost",
-  port: 5433,
+  port: 5432,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
