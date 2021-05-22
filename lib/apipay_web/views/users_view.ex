0db181d12c1a2f43333a5e64b5622a63a5844210
joaoeliandro <eliandrogermano@gmail.com>
2021-05-22 02:33:47 -0300

    Added view account to created user

--- lib/apipay_web/views/users_view.ex
+++ lib/apipay_web/views/users_view.ex
@@ -1,13 +1,28 @@
 defmodule ApipayWeb.UsersView do
-  alias Apipay.User
+  alias Apipay.{Account, User}
 
-  def render("create.json", %{user: %User{id: id, name: name, nickname: nickname}}) do
+  def render("create.json", %{
+        user: %User{
+          account: %Account{id: account_id, balance: balance},
+          id: id,
+          email: email,
+          name: name,
+          age: age,
+          nickname: nickname
+        }
+      }) do
     %{
       message: "User created!",
       user: %{
         id: id,
         name: name,
-        nickname: nickname
+        age: age,
+        email: email,
+        nickname: nickname,
+        account: %{
+          id: account_id,
+          balance: balance
+        }
       }
     }
   end
