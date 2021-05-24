b2723e778b5851b21e1c27f2dd96f24611e46d64
joaoeliandro <eliandrogermano@gmail.com>
2021-05-24 05:47:22 -0300

    Added withdraw delegate on facade

--- lib/apipay.ex
+++ lib/apipay.ex
@@ -1,11 +1,10 @@
 defmodule Apipay do
   alias Apipay.Users.Create, as: UserCreate
 
-  alias Apipay.Accounts.Deposit
-  # alias Apipay.Accounts.Withdraw
+  alias Apipay.Accounts.{Deposit, Withdraw}
 
   defdelegate create_user(params), to: UserCreate, as: :call
 
   defdelegate deposit(params), to: Deposit, as: :call
-  # defdelegate withdraw(params), to: Withdraw, as: :call
+  defdelegate withdraw(params), to: Withdraw, as: :call
 end
