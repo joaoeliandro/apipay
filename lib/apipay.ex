c4296e4d0cc09bf008cdf3dc53d74b7b8b1714b1
joaoeliandro <eliandrogermano@gmail.com>
2021-05-24 08:17:16 -0300

    Added transaction delegate on facade

--- lib/apipay.ex
+++ lib/apipay.ex
@@ -1,10 +1,11 @@
 defmodule Apipay do
   alias Apipay.Users.Create, as: UserCreate
 
-  alias Apipay.Accounts.{Deposit, Withdraw}
+  alias Apipay.Accounts.{DepositService, TransactionService, WithdrawService}
 
   defdelegate create_user(params), to: UserCreate, as: :call
 
-  defdelegate deposit(params), to: Deposit, as: :call
-  defdelegate withdraw(params), to: Withdraw, as: :call
+  defdelegate deposit(params), to: DepositService, as: :call
+  defdelegate withdraw(params), to: WithdrawService, as: :call
+  defdelegate transaction(params), to: TransactionService, as: :call
 end
