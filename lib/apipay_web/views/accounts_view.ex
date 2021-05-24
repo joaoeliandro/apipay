f2e15e3cfe03c1fe4507542f51600f746d575544
joaoeliandro <eliandrogermano@gmail.com>
2021-05-24 08:18:48 -0300

    Added transaction render json view

--- lib/apipay_web/views/accounts_view.ex
+++ lib/apipay_web/views/accounts_view.ex
@@ -1,5 +1,5 @@
 defmodule ApipayWeb.AccountsView do
-  alias Apipay.Account
+  alias Apipay.{Account, Transaction}
 
   def render("update.json", %{account: %Account{id: account_id, balance: balance}}) do
     %{
@@ -7,6 +7,31 @@ defmodule ApipayWeb.AccountsView do
       account: %{
         id: account_id,
         balance: balance
+      }
+    }
+  end
+
+  def render("transaction.json", %{
+        transaction: %Transaction{
+          id: transaction_id,
+          to: to_account,
+          from: from_account,
+          value: value
+        }
+      }) do
+    %{
+      message: "Transaction done successfully!",
+      transaction: %{
+        id: transaction_id,
+        from: %{
+          id: from_account.id,
+          balance: from_account.balance
+        },
+        to: %{
+          id: to_account.id,
+          balance: to_account.balance
+        },
+        value: value
       }
     }
   end
