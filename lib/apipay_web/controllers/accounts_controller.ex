d6963ad3ae362fbb7a7def4292812fb1a750ebf1
joaoeliandro <eliandrogermano@gmail.com>
2021-05-25 19:11:55 -0300

    Added router all transactions and multi proccess async

--- lib/apipay_web/controllers/accounts_controller.ex
+++ lib/apipay_web/controllers/accounts_controller.ex
@@ -22,10 +22,22 @@ defmodule ApipayWeb.AccountsController do
   end
 
   def transaction(conn, params) do
-    with {:ok, %Transaction{} = transaction} <- Apipay.transaction(params) do
+    task = Task.async(fn -> Apipay.transaction(params) end)
+
+    with {:ok, %Transaction{} = transaction} <- Task.await(task) do
       conn
       |> put_status(:ok)
       |> render("transaction.json", transaction: transaction)
+    end
+  end
+
+  def all_transactions(conn, _params) do
+    task = Task.async(fn -> Apipay.all_transaction() end)
+
+    with {:ok, transactions} <- Task.await(task) do
+      conn
+      |> put_status(:ok)
+      |> render("transactions.json", transactions: transactions)
     end
   end
 end
